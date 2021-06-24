2c8ddb6eaf3367d05dad7e863753725b70c99964
hzm2016 <2539722953@qq.com>
2021-06-24 15:32:28 +0800

    update code

--- control/motor_control/test_gyems/test.cpp
+++ control/motor_control/test_gyems/test.cpp
@@ -43,18 +43,18 @@ int main()
     // Initial hardware ::: can device
     ////////////////////////////////////////////////////////
 
-    // std::cout << "Initial Can0 and Can1 !!!" << std::endl;  
-    // CANDevice can0((char *) "can0");  
-    // can0.begin(); 
-    // CANDevice can1((char *) "can1");   
-    // can1.begin(); 
+    std::cout << "Initial Can0 and Can1 !!!" << std::endl;  
+    CANDevice can0((char *) "can0");  
+    can0.begin(); 
+    CANDevice can1((char *) "can1");  
+    can1.begin(); 
 
-    // Gcan motor_1(can0); 
-    // Gcan motor_2(can1);   
-    // motor_1.begin(); 
-    // motor_2.begin(); 
+    Gcan motor_1(can0); 
+    Gcan motor_2(can1);   
+    motor_1.begin(); 
+    motor_2.begin(); 
 
-    // std::cout << "Enable motor!!!" << std::endl; 
+    std::cout << "Enable motors !!!" << std::endl; 
     
     ////////////////////////////////////////////////////////
     // Load path from txt file
@@ -103,11 +103,11 @@ int main()
     ////////////////////////////////////////////////////////
     // Initial Calibration
     ////////////////////////////////////////////////////////
-    // double theta_1_initial = motor_1.read_sensor(1); 
-    // printf(" Motor position 1: %f\n", theta_1_initial); 
+    double theta_1_initial = motor_1.read_sensor(1); 
+    printf(" Motor 1 initial position: %f\n", theta_1_initial); 
 
-    // double theta_2_initial = motor_2.read_sensor(2); 
-    // printf(" Motor position 2: %f\n", theta_2_initial); 
+    double theta_2_initial = motor_2.read_sensor(2); 
+    printf(" Motor 2 initial position: %f\n", theta_2_initial); 
 
     ////////////////////////////////////////////////////////
     // Impedance Parameters
@@ -119,8 +119,8 @@ int main()
     double torque_lower_bound = -1; 
     double torque_upper_bound = 1; 
     
-    double ctl_ratio = 1; 
-    double ctl_ratio = 1; 
+    double ctl_ratio_1 = 1.0;
+    double ctl_ratio_2 = 1.0;  
 
     double theta_1_t = 0.0; 
     double theta_2_t = 0.0; 
@@ -140,7 +140,7 @@ int main()
     double pos_1 = 0.0; 
     double pos_2 = 0.0; 
 
-    for(int index=0;index<Num_waypoints;index=index+1) 
+    for(int index=0; index<Num_waypoints; index=index+1) 
     {
         double theta_1_e = theta_1_list[index]; 
         double theta_2_e = theta_2_list[index]; 
@@ -156,24 +156,24 @@ int main()
             d_theta_2_e = (theta_2_list[index] - theta_2_list[index-1])/d_t; 
         }
         
-        // theta_1_t = motor_1.read_sensor(1) - theta_1_initial; 
-        // theta_2_t = motor_2.read_sensor(2) - theta_2_initial; 
+        theta_1_t = motor_1.read_sensor(1) - theta_1_initial; 
+        theta_2_t = motor_2.read_sensor(2) - theta_2_initial; 
 
-        // d_theta_1_t = motor_1.read_sensor(1) - theta_1_initial; 
-        // d_theta_2_t = motor_2.read_sensor(2) - theta_2_initial; 
+        d_theta_1_t = motor_1.read_sensor(1) - theta_1_initial; 
+        d_theta_2_t = motor_2.read_sensor(2) - theta_2_initial; 
 
         // set torque control command 
         torque_1 = clip(-1 * K_p * (theta_1_e - theta_1_t) - K_d * (d_theta_1_e - d_theta_1_t), torque_lower_bound, torque_upper_bound); 
         torque_2 = clip(-1 * K_p * (theta_2_e - theta_2_t) - K_d * (d_theta_2_e - d_theta_2_t), torque_lower_bound, torque_upper_bound); 
 
-        pos_1 = motor_1.set_torque(1, torque_1, &d_theta_1_t, &torque_1_t); 
-        pos_2 = motor_2.set_torque(2, torque_2, &d_theta_2_t, &torque_2_t); 
+        // pos_1 = motor_1.set_torque(1, torque_1, &d_theta_1_t, &torque_1_t); 
+        // pos_2 = motor_2.set_torque(2, torque_2, &d_theta_2_t, &torque_2_t); 
 
         ////////////////////////////////////////////////////////
         // Save Data
         ////////////////////////////////////////////////////////
-        OutFileTorque1 << torque_1_t << "\n"; 
-        OutFileTorque2 << torque_2_t << "\n"; 
+        // OutFileTorque1 << torque_1_t << "\n"; 
+        // OutFileTorque2 << torque_2_t << "\n"; 
 
         OutFileAngle1 << theta_1_t << "\n"; 
         OutFileAngle2 << theta_2_t << "\n"; 
