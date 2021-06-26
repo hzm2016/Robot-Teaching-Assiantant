628291957d5915dd0a93b6dca287b1ce891c9cbf
hzm2016 <2539722953@qq.com>
2021-06-26 22:14:12 +0800

    update

--- control/ctl_main.py
+++ control/ctl_main.py
@@ -1,6 +1,6 @@
-import cv2
-from control.vision_capture.main_functions import capture_image
-from control.path_planning.path_generate import *
+# import cv2
+# from control.vision_capture.main_functions import capture_image
+# from control.path_planning.path_generate import *
 
 if __name__ == "__main__":
 	
@@ -10,39 +10,41 @@ if __name__ == "__main__":
 	
 	import socket
 	
-	address = ('127.0.0.1', 5005)  # 服务端地址和端口
+	address = ('192.168.1.182', 5005)  # 服务端地址和端口
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind(address)  # 绑定服务端地址和端口
 	s.listen(5)
 	conn, addr = s.accept()  # 返回客户端地址和一个新的 socket 连接
 	print('[+] Connected with', addr)
 	while True:
+		conn, addr = s.accept()  # 返回客户端地址和一个新的 socket 连接
+		print('[+] Connected with', addr)
 		data = conn.recv(1024)  # buffersize 等于 1024
 		data = data.decode()
 		if not data:
 			break
 		print('[Received]', data)
-		send = input('Input: ')
-		conn.sendall(send.encode())
+		# send = input('Input: ')
+		# conn.sendall(send.encode())
 	conn.close()
 	s.close()
 	
-	import socket
-	import sys
-	
-	address = ('127.0.0.1', 5005)  # 服务端地址和端口
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	try:
-		s.connect(address)  # 尝试连接服务端
-	except Exception:
-		print('[!] Server not found ot not open')
-		sys.exit()
-	while True:
-		trigger = input('Input: ')
-		s.sendall(trigger.encode())
-		data = s.recv(1024)
-		data = data.decode()
-		print('[Recieved]', data)
-		if trigger == '###':  # 自定义结束字符串
-			break
-	s.close()
+	# import socket
+	# import sys
+	#
+	# address = ('127.0.0.1', 5005)  # 服务端地址和端口
+	# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# try:
+	# 	s.connect(address)  # 尝试连接服务端
+	# except Exception:
+	# 	print('[!] Server not found ot not open')
+	# 	sys.exit()
+	# while True:
+	# 	trigger = input('Input: ')
+	# 	s.sendall(trigger.encode())
+	# 	data = s.recv(1024)
+	# 	data = data.decode()
+	# 	print('[Recieved]', data)
+	# 	if trigger == '###':  # 自定义结束字符串
+	# 		break
+	# s.close()
