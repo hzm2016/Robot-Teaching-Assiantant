eb2caefedab5768138134688799833053aa5d6d6
hzm2016 <2539722953@qq.com>
2021-07-12 23:20:54 +0800

    update

--- control/motor_control/main.cpp
+++ control/motor_control/main.cpp
@@ -228,16 +228,16 @@ int get_demonstration(double theta_1_initial, double theta_2_initial)
 }
 
 
-int load_path_data()
+int load_path_data(double &theta_1_list[], double &theta_2_list[], int Num_waypoints)
 {
     ////////////////////////////////////////////////////////
     // Load path from txt file
-    ////////////////////////////////////////////////////////
-    int Num_waypoints = 19999;   
+    ////////////////////////////////////////////////////////   
     int index_point = 0;    
 
-    double theta_1_list[Num_waypoints];    
-    double theta_2_list[Num_waypoints];     
+    // int Num_waypoints = 19999; 
+    // double theta_1_list[Num_waypoints];    
+    // double theta_2_list[Num_waypoints];     
 
     string input_path = "angle_list.txt";    
     ifstream input_angle_list;    
@@ -253,7 +253,7 @@ int load_path_data()
         printf("theta 1: %f\n", theta_1_list[index_point]);  
 
         theta_2_list[index_point] = atof(angle_list[1].c_str());   
-        printf("theta 2: %f\n", theta_2_list[index_point]); 
+        printf("theta 2: %f\n", theta_2_list[index_point]);  
 
         index_point += 1;  
     }
@@ -262,178 +262,185 @@ int load_path_data()
 }
 
 
-// int run_one_loop(double stiffness, double damping)
-// {
-//     printf("Input stiffness parameters :: %f\n", stiffness);  
-//     printf("Input damping parameters :: %f\n", damping);  
+int run_one_loop(double stiffness, double damping, double theta_1_initial, double theta_2_initial, int Num_waypoints) 
+{
+    //////////////////////////////////////////////////////// 
+    // give way points :::: 
+    //////////////////////////////////////////////////////// 
 
-//     ////////////////////////////////////////////////////////
-//     // Initial hardware ::: can device
-//     ////////////////////////////////////////////////////////
+    printf("Input stiffness :: %f\n", stiffness);  
+    printf("Input damping :: %f\n", damping);  
 
-//     std::cout << "Initial Can0 and Can1 !!!" << std::endl;  
-//     CANDevice can0((char *) "can0");  
-//     can0.begin(); 
-//     CANDevice can1((char *) "can1");  
-//     can1.begin(); 
+    double K_p_1 = stiffness;   // 16
+	double K_d_1 = damping;   // 0.8
 
-//     Gcan motor_1(can1);   
-//     Gcan motor_2(can0);   
-//     motor_1.begin();   
-//     motor_2.begin();   
+    double K_p_2 = stiffness;   
+	double K_d_2 = damping;   
 
-//     std::cout << "Enable Motors Done!!!" << std::endl;   
+    ////////////////////////////////////////////////////////
+    // Initial offset angles ::: input 
+    //////////////////////////////////////////////////////// 
+    // double theta_1_initial = -0.294288;   
+    // double theta_2_initial = 0.402938;   
 
-//     ////////////////////////////////////////////////////////
-//     // Define file to store data
-//     ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////
+    // Initial hardware ::: can device
+    ////////////////////////////////////////////////////////
 
-//     // string root_path = "home/zhimin/code/8_nus/robotic-teaching/control/motor_control"; 
-//     string root_path = "data/"; 
+    std::cout << "Initial Can0 and Can1 !!!" << std::endl;  
+    CANDevice can0((char *) "can0");  
+    can0.begin();  
+    CANDevice can1((char *) "can1");  
+    can1.begin(); 
 
-//     string output_torque = root_path + "torque_list.txt"; 
-//     ofstream OutFileTorque(output_torque); 
-//     OutFileTorque << "torque_1" << " " << "torque_2" << "\n";  
+    Gcan motor_1(can1);   
+    Gcan motor_2(can0);   
+    motor_1.begin();   
+    motor_2.begin();   
 
-//     // string output_torque_1 = root_path + "torque_list_1.txt";   
-//     // ofstream OutFileTorque1;   
-//     // OutFileTorque1.open(output_torque_1.c_str());  
+    std::cout << "Enable Motors Done!!!" << std::endl;   
 
-//     string output_angle = root_path + "angle_list.txt";    
-//     ofstream OutFileAngle(output_angle);    
-//     OutFileAngle << "angle_1" << " " << "angle_2" << "\n";    
- 
-//     // string output_angle_2 = root_path + "angle_list_2.txt"; 
-//     // ofstream OutFileAngle2(output_angle_2); 
+    ////////////////////////////////////////////////////////
+    // Define file to store data
+    ////////////////////////////////////////////////////////
 
-//     string output_vel = root_path + "angle_vel_list.txt";  
-//     ofstream OutFileVel(output_vel);   
-//     OutFileVel << "vel_1" << " " << "vel_2" << "\n";   
+    // string root_path = "home/zhimin/code/8_nus/robotic-teaching/control/motor_control"; 
+    string root_path = ""; 
 
-//     // string output_vel_2 = root_path + "angle_vel_list_2.txt"; 
-//     // ofstream OutFileVel2(output_vel_2); 
+    string output_torque = root_path + "real_torque_list.txt"; 
+    ofstream OutFileTorque(output_torque); 
+    OutFileTorque << "torque_1" << "," << "torque_2" << "\n";  
 
-//     ////////////////////////////////////////////////////////
-//     // Initial offset angles ::: input 
-//     //////////////////////////////////////////////////////// 
-//     double theta_1_initial = -0.294288;   
-//     double theta_2_initial = 0.402938;   
+    string output_angle = root_path + "real_angle_list.txt";    
+    ofstream OutFileAngle(output_angle);  
+    OutFileAngle << "angle_1" << "," << "angle_2" << "\n";   
 
-//     ////////////////////////////////////////////////////////
-//     // Impedance Parameters ::: input 
-//     ////////////////////////////////////////////////////////
-// 	double K_p_1 = 16;   
-// 	double K_d_1 = 0.8;   
-//     double K_p_2 = 14;   
-// 	double K_d_2 = 0.8;   
-// 	double K_i = 0.0;   
+    string output_vel = root_path + "real_angle_vel_list.txt";  
+    ofstream OutFileVel(output_vel);   
+    OutFileVel << "vel_1" << "," << "vel_2" << "\n";  
 
-//     double torque_lower_bound = -2.5;   
-//     double torque_upper_bound = 2.5;   
+    ////////////////////////////////////////////////////////
+    // Impedance Parameters ::: input 
+    ////////////////////////////////////////////////////////
+
+    double torque_lower_bound = -2.5;   
+    double torque_upper_bound = 2.5;   
     
-//     // depends on the relation of torque and current
-//     double ctl_ratio_1 = - 2000.0/32;   
-//     double ctl_ratio_2 = 2000.0/32;   
-//     double d_t = 0.001;   
+    double ctl_ratio_1 = - 2000.0/32;   
+    double ctl_ratio_2 = 2000.0/32;  
+
+    double d_t = 0.001;   
 
     
-//     double theta_1_t = 0.0;   
-//     double theta_2_t = 0.0;   
+    double theta_1_t = 0.0;   
+    double theta_2_t = 0.0;   
 
-//     double d_theta_1_t = 0.0;    
-//     double d_theta_2_t = 0.0;    
+    double d_theta_1_t = 0.0;    
+    double d_theta_2_t = 0.0;    
 
-//     double theta_1_e = 0.0;  
-//     double theta_2_e = 0.0;  
+    double theta_1_e = 0.0;  
+    double theta_2_e = 0.0;  
 
-//     double d_theta_1_e = 0.0;  
-//     double d_theta_2_e = 0.0;  
+    double d_theta_1_e = 0.0;  
+    double d_theta_2_e = 0.0;  
 
-//     double torque_1 = 0.0;  
-//     double torque_2 = 0.0;  
+    double torque_1 = 0.0;  
+    double torque_2 = 0.0;  
 
-//     double torque_1_t = 0.0;  
-//     double torque_2_t = 0.0;  
+    double torque_1_t = 0.0;  
+    double torque_2_t = 0.0;  
 
-//     double pos_1 = 0.0;  
-//     double pos_2 = 0.0;  
+    double pos_1 = 0.0;  
+    double pos_2 = 0.0;  
     
 
-//     // for(int index=0; index<Num_waypoints; index=index+1) 
-//     // {
-//     //     theta_1_e = theta_1_list[index]; 
-//     //     theta_2_e = theta_2_list[index]; 
+    // for(int index=0; index<Num_waypoints; index=index+1) 
+    // {
+    //     theta_1_e = theta_1_list[index]; 
+    //     theta_2_e = theta_2_list[index]; 
 
-//     //     cout << "index" << index << "\n";
-//     //     cout << "theta_1_e" << theta_1_e << "\n";
-//     //     cout << "theta_2_e" << theta_2_e << "\n";
-//     // }
+    //     cout << "index" << index << "\n";
+    //     cout << "theta_1_e" << theta_1_e << "\n";
+    //     cout << "theta_2_e" << theta_2_e << "\n";
+    // }
 
-//     ///////////////////////////////////////////////////////
-//     // avoid large motion at starting points
-//     ///////////////////////////////////////////////////////
-//     for(int index=0; index<5; index=index+1) 
-//     {
-//         pos_1 = motor_1.set_torque(2, 0.0, &d_theta_1_t, &torque_1_t); 
-//         pos_2 = motor_2.set_torque(1, 0.0, &d_theta_2_t, &torque_2_t); 
-//     }
+    // int Num_waypoints = 19999; 
+    double theta_1_list[Num_waypoints];    
+    double theta_2_list[Num_waypoints];   
 
-//     for(int index=0; index<Num_waypoints; index=index+1) 
-//     {
-//         theta_1_e = theta_1_list[index]; 
-//         theta_2_e = theta_2_list[index]; 
+    ///////////////// load data ///////////////////////////
+    ///////////////////////////////////////////////////////
 
-//         if(index==0) 
-//         {
-//             d_theta_1_e = 0.0; 
-//             d_theta_2_e = 0.0; 
-//         }
-//         else 
-//         {
-//             d_theta_1_e = (theta_1_list[index] - theta_1_list[index-1])/d_t; 
-//             d_theta_2_e = (theta_2_list[index] - theta_2_list[index-1])/d_t; 
-//         }
+    load_path_data(theta_1_list, theta_2_list, Num_waypoints);   
+
+    printf("Theta_1_list :: %f\n", theta_1_list[0]);    
+    printf("Theta_2_list :: %f\n", theta_2_list[0]);    
+
+    ///////////////////////////////////////////////////////
+    // avoid large motion at starting points
+    ///////////////////////////////////////////////////////
+    // for(int index=0; index<5; index=index+1) 
+    // {
+    //     pos_1 = motor_1.set_torque(2, 0.0, &d_theta_1_t, &torque_1_t); 
+    //     pos_2 = motor_2.set_torque(1, 0.0, &d_theta_2_t, &torque_2_t); 
+    // }
+
+    // for(int index=0; index<Num_waypoints; index=index+1) 
+    // {
+    //     theta_1_e = theta_1_list[index];  
+    //     theta_2_e = theta_2_list[index];  
+
+    //     if(index==0) 
+    //     {
+    //         d_theta_1_e = 0.0; 
+    //         d_theta_2_e = 0.0; 
+    //     }
+    //     else 
+    //     {
+    //         d_theta_1_e = (theta_1_list[index] - theta_1_list[index-1])/d_t; 
+    //         d_theta_2_e = (theta_2_list[index] - theta_2_list[index-1])/d_t; 
+    //     }
         
-//         // read joint angles
-//         theta_1_t = motor_1.read_sensor(2) - theta_1_initial;  
-//         theta_2_t = -1 * (motor_2.read_sensor(1) + theta_1_t - theta_2_initial);  
+    //     // read joint angles 
+    //     theta_1_t = motor_1.read_sensor(2) - theta_1_initial;   
+    //     theta_2_t = -1 * (motor_2.read_sensor(1) + theta_1_t - theta_2_initial);  
 
-//         /////////////////////////////////////////////////////
-//         // set torque control command 
-//         /////////////////////////////////////////////////////
-//         torque_1 = clip(- K_p_1 * (theta_1_e - theta_1_t) - K_d_1 * (d_theta_1_e - d_theta_1_t), torque_lower_bound, torque_upper_bound) * ctl_ratio_1; 
-//         torque_2 = clip(- K_p_2 * (theta_2_e - theta_2_t) - K_d_2 * (d_theta_2_e - d_theta_2_t), torque_lower_bound, torque_upper_bound) * ctl_ratio_2; 
+    //     /////////////////////////////////////////////////////
+    //     // set torque control command 
+    //     ///////////////////////////////////////////////////// 
+    //     torque_1 = clip(- K_p_1 * (theta_1_e - theta_1_t) - K_d_1 * (d_theta_1_e - d_theta_1_t), torque_lower_bound, torque_upper_bound) * ctl_ratio_1; 
+    //     torque_2 = clip(- K_p_2 * (theta_2_e - theta_2_t) - K_d_2 * (d_theta_2_e - d_theta_2_t), torque_lower_bound, torque_upper_bound) * ctl_ratio_2; 
 
-//         double torque_1_o = - K_p_1 * (theta_1_e - theta_1_t) - K_d_1 * (d_theta_1_e - d_theta_1_t); 
-//         double torque_2_o = - K_p_2 * (theta_2_e - theta_2_t) - K_d_2 * (d_theta_2_e - d_theta_2_t); 
+    //     double torque_1_o = - K_p_1 * (theta_1_e - theta_1_t) - K_d_1 * (d_theta_1_e - d_theta_1_t);  
+    //     double torque_2_o = - K_p_2 * (theta_2_e - theta_2_t) - K_d_2 * (d_theta_2_e - d_theta_2_t); 
 
-//         // printf("input_torque_1_t: %f\n", torque_1); 
-//         // printf("input_torque_2_t: %f\n", torque_1); 
+    //     // printf("input_torque_1_t: %f\n", torque_1); 
+    //     // printf("input_torque_2_t: %f\n", torque_1); 
 
-//         pos_1 = motor_1.set_torque(2, torque_1, &d_theta_1_t, &torque_1_t); 
-//         pos_2 = motor_2.set_torque(1, torque_2, &d_theta_2_t, &torque_2_t); 
+    //     pos_1 = motor_1.set_torque(2, torque_1, &d_theta_1_t, &torque_1_t); 
+    //     pos_2 = motor_2.set_torque(1, torque_2, &d_theta_2_t, &torque_2_t); 
 
 
-//         ////////////////////////////////////////////////////////
-//         // Save Data
-//         ////////////////////////////////////////////////////////
+    //     ////////////////////////////////////////////////////////
+    //     // Save Data
+    //     ////////////////////////////////////////////////////////
 
-//         OutFileAngle << theta_1_t << " " << theta_2_t << "\n";  
+    //     OutFileAngle << theta_1_t << "," << theta_2_t << "\n";  
 
-//         OutFileTorque << torque_1_o << " " << torque_2_o << "\n"; 
+    //     OutFileTorque << torque_1_o << "," << torque_2_o << "\n"; 
 
-//         OutFileVel << d_theta_1_t << " " << d_theta_2_t << "\n"; 
-//     }
+    //     OutFileVel << d_theta_1_t << "," << d_theta_2_t << "\n"; 
+    // }
     
-//     OutFileTorque.close();   
-//     OutFileAngle.close();    
-//     OutFileVel.close();   
+    // OutFileTorque.close();   
+    // OutFileAngle.close();    
+    // OutFileVel.close();   
 
-//     motor_1.pack_stop_cmd(2);  
-//     motor_2.pack_stop_cmd(1);  
+    // motor_1.pack_stop_cmd(2);  
+    // motor_2.pack_stop_cmd(1);  
 
-//     return 0;  
-// }
+    return 0;  
+}
 
 
 namespace py = pybind11;
@@ -498,6 +505,14 @@ PYBIND11_MODULE(motor_control, m) {
 
         Some other explanation about the add function.
     )pbdoc"); 
+
+    m.def("run_one_loop", &run_one_loop, R"pbdoc(
+        run_one_loop
+
+        Some other explanation about the add function.
+    )pbdoc"); 
+
+    // run_one_loop(double stiffness, double damping, double theta_1_initial, double theta_2_initial, int Num_waypoints)
 
 
 #ifdef VERSION_INFO
--- control/motor_control/test_main.py
+++ control/motor_control/test_main.py
@@ -1,20 +1,18 @@
 import motor_control
 import copy
-from control.path_planning.plot_path import *
+import 
+
 
 # motor_control.reset(theta_1_initial, theta_2_initial)
 # print(theta_1_initial)
 # print(motor_control.add(1, 2))
+
 # motor_control.load_path_data() 
 
-# theta_1_initial = motor_control.read_initial_angle_1()
-# print("theta_1_initial :::", theta_1_initial)
-# theta_2_initial = motor_control.read_initial_angle_2()
-# print("theta_2_initial :::", theta_2_initial)
-#
-# motor_control.get_demonstration(theta_1_initial, theta_2_initial)
+theta_1_initial = motor_control.read_initial_angle_1()
+print("theta_1_initial :::", theta_1_initial) 
+theta_2_initial = motor_control.read_initial_angle_2()
+print("theta_2_initial :::", theta_2_initial) 
 
-plot_real_2d_path(root_path='../',
-                  # file_name='angle_list.txt'
-                  file_name='demonstrated_angle_list.txt'
-)
+# motor_control.get_demonstration(theta_1_initial, theta_2_initial)
+motor_control.run_one_loop(16, 0.8, theta_1_initial, theta_2_initial, 19999) 
