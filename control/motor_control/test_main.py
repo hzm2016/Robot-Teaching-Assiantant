4e4e2bb6b666d3fb70a35e24c5da95414304bc9e
hzm2016 <2539722953@qq.com>
2021-07-12 20:22:05 +0800

    update

--- control/motor_control/test_main.py
+++ control/motor_control/test_main.py
@@ -1,16 +1,20 @@
 import motor_control
 import copy
-
+from control.path_planning.plot_path import *
 
 # motor_control.reset(theta_1_initial, theta_2_initial)
 # print(theta_1_initial)
 # print(motor_control.add(1, 2))
-
 # motor_control.load_path_data() 
 
-theta_1_initial = motor_control.read_initial_angle_1()
-print("theta_1_initial :::", theta_1_initial) 
-theta_2_initial = motor_control.read_initial_angle_2()
-print("theta_2_initial :::", theta_2_initial) 
+# theta_1_initial = motor_control.read_initial_angle_1()
+# print("theta_1_initial :::", theta_1_initial)
+# theta_2_initial = motor_control.read_initial_angle_2()
+# print("theta_2_initial :::", theta_2_initial)
+#
+# motor_control.get_demonstration(theta_1_initial, theta_2_initial)
 
-motor_control.get_demonstration(theta_1_initial, theta_2_initial)
+plot_real_2d_path(root_path='../',
+                  # file_name='angle_list.txt'
+                  file_name='demonstrated_angle_list.txt'
+)
