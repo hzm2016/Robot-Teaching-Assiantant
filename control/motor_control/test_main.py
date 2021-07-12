11f17c7111f8aa8aa090614ac39fcfaa7b0cd254
hzm2016 <2539722953@qq.com>
2021-07-12 18:48:34 +0800

    update

--- control/motor_control/test_main.py
+++ control/motor_control/test_main.py
@@ -1,8 +1,13 @@
 import motor_control
 
-# theta_1_initial = 1.0
-# theta_2_initial = 2.0
+
 # motor_control.reset(theta_1_initial, theta_2_initial)
 # print(theta_1_initial)
 # print(motor_control.add(1, 2))
-motor_control.load_path_data() 
+
+# motor_control.load_path_data() 
+
+theta_1_initial = motor_control.read_initial_angle_1() 
+print("theta_1_initial :::", theta_1_initial) 
+theta_2_initial = motor_control.read_initial_angle_2() 
+print("theta_2_initial :::", theta_2_initial) 
