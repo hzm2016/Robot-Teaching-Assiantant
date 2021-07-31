1c6b11a98fd73b42dc32bec9124d2ab86c63390e
hzm2016 <2539722953@qq.com>
2021-07-31 14:36:58 +0800

    update

--- control/server_main.py
+++ control/server_main.py
@@ -3,6 +3,65 @@ import numpy as np
 import math
 import os
 from motor_control import motor_control
+L_1 = 300
+L_2 = 250
+action_dim = 2
+DIST_THREHOLD = 0.05
+
+
+def reset_and_calibration():
+	print("Please make sure two links are at zero position !!!")
+	angle_initial = np.zeros(action_dim)
+	
+	angle_initial[0] = motor_control.read_initial_angle_1()
+	angle_initial[1] = motor_control.read_initial_angle_2()
+	print("theta_1_initial :::", theta_1_initial)
+	print("theta_2_initial :::", theta_2_initial)
+	
+	# theta_1_initial = -0.336998
+	# theta_2_initial = 0.426342
+	return angle_initial
+
+
+def get_observation(angle_initial=np.array([-0.336998, 0.426342])):
+	"""
+		obtain joint angles and cartesian state
+	"""
+	angle = np.zeros(action_dim)
+	point = np.zeros(action_dim)
+	
+	angle[0] = motor_control.read_angle_1(angle_initial[0])
+	angle[1] = motor_control.read_angle_2(angle_initial[1], angle[0].copy())
+	print("Joint angles (rad) :", angle)
+	
+	point[0] = L_1 * math.cos(angle[0]) + L_2 * math.cos(angle[0] + angle[1])
+	point[1] = L_1 * math.sin(angle[0]) + L_2 * math.sin(angle[0] + angle[1])
+	print("Position (m) :", point)
+	
+	return angle, point
+
+
+def move_to_initial_point(target_point):
+	curr_angle, curr_point = get_observation()
+	dist = np.linalg.norm((curr_point - target_point), ord=2)
+	while dist > DIST_THREHOLD:
+		motor_control.move_to_target(target_point)
+		dist = np.linalg.norm((curr_point - target_point), ord=2)
+	done = True
+	return done, dist
+
+
+def train():
+	# ######################################################
+	# ############## get current state #####################
+	# ######################################################
+	pass
+	
+
+def eval():
+	
+	pass
+
 
 if __name__ == "__main__":
 	theta_1_initial = -0.336998
@@ -13,8 +72,8 @@ if __name__ == "__main__":
 	# ######################################################
 	# ############## get current state #####################
 	# ######################################################
-	theta_1_t = motor_control.read_angle_1(theta_1_initial) 
-	print("theta_1_t :::", theta_1_t) 
+	theta_1_t = motor_control.read_angle_1(theta_1_initial)
+	print("theta_1_t :::", theta_1_t)
 	theta_2_t = motor_control.read_angle_2(theta_2_initial, theta_1_t) 
 	print("theta_2_t :::", theta_2_t) 
 
