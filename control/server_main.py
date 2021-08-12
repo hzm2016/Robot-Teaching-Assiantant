72c6df81a2c5ff2c7af0cbf5f8a26955b3d2212e
hzm2016 <2539722953@qq.com>
2021-08-12 22:38:46 +0800

    update

--- control/server_main.py
+++ control/server_main.py
@@ -433,8 +433,9 @@ def load_word_path(word_name=None):
 
 if __name__ == "__main__":  
 
-    word_path = load_word_path(word_name='ren') 
-    write_word(word_path, impedance_params=np.array([35.0, 30.0, 0.4, 0.1]))  
+    write_name = 'ren'
+    word_path = load_word_path(word_name=write_name) 
+    write_word(word_path, impedance_params=np.array([35.0, 30.0, 0.4, 0.1]), word_name=write_name)   
 
     # from path_planning.plot_path import * 
     # plot_real_2d_path(
